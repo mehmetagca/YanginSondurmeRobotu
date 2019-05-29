fa53e51f22ecd79963512403602a0259579e1540
senertaner <tanersener35@gmail.com>
2019-05-29 18:17:51 +0300

    Added servoFlame.h

---
+++ YanginSondurmeRobotu/MegaMotor4lu/servoFlame.h
@@ -0,0 +1,46 @@
+#include <Servo.h>
+ Servo myservomotor; // sınıfın bir örneğini aldık
+ const int sensorMin = 0; // sensor minimum
+ const int sensorMax = 1024; // sensor maximum
+ int derece;
+   
+void servoSetup(){
+  myservomotor.attach(12);  // arduinonun 8. pinini çıkış yaptık.
+  myservomotor.write(0);  // motora ilk 0.derecesinden başlaması komutunu verdik
+}
+
+byte isFireClose(){
+  byte returnValue = 0; 
+  int sensorReading = analogRead(A0);
+  int range = map(sensorReading, sensorMin, sensorMax, 0, 100);    
+  Serial.print("range  : ");
+  Serial.println(range);
+  if(range < 4)
+  {
+    derece=90;  // 180 derecelik açı yap
+    myservomotor.write(derece);
+    returnValue = 1;
+    //delay(10);   // her 1 derece açıyı 10 milisaniyede yapacak  
+  }
+  else{
+    derece=0;
+    myservomotor.write(derece);
+    
+  }
+  return returnValue;
+}
+
+/*
+#include "servoFlame.h"
+
+void setup()
+{
+ servoSetup();
+}
+
+void loop()
+{
+  Serial.print("isFireClose() : ");
+  Serial.print(isFireClose());
+}
+*/
