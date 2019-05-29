a63e8232a85e9f9d94b64e7f1c7328c253591b22
Bülent AKPINAR <bakpinar@hotmail.com>
2019-05-29 15:12:02 +0300

    updated signal artis if control

--- YanginSondurmeRobotu/MegaMotor4lu/sinyalArtis.h
+++ YanginSondurmeRobotu/MegaMotor4lu/sinyalArtis.h
@@ -35,9 +35,9 @@
   Serial.print("kucuk : ");
   Serial.println(kucuk);
 
-	if (buyuk > kucuk)
+	if ((buyuk - kucuk)>2)
 		return 1; // return 1 : sinyal artıyor
-	else if(kucuk > buyuk) 
+	else if((kucuk - buyuk)>2) 
 		return 0; // return 0 : sinyal azalıyor
   else
     return -1; // return -1 : sinyal değerleri eşit     
